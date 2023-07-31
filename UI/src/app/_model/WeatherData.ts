6ef3ec8c66a9d95078b3a9828b45fbb2c7a4b9ac
Max <59653155+MaxPopaylo@users.noreply.github.com>
2023-07-31 16:18:16 +0300

    Update WeatherData.ts

--- UI/src/app/_model/WeatherData.ts
+++ UI/src/app/_model/WeatherData.ts
@@ -1,13 +1,6 @@
-export class WeatherData {
+export interface WeatherData {
   value: number;
   raining: boolean;
   date: Date;
   sensor_name: string;
-
-  constructor(value: number, raining: boolean, date: Date, sensor_name: string) {
-    this.value = value;
-    this.raining = raining;
-    this.date = date;
-    this.sensor_name = sensor_name;
-  }
 }
