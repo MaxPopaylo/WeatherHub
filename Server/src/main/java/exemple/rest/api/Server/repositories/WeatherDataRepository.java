ca2ad8785294d2e092a087ed7fcc92f2f8219b51
MaxPopaylo <max.popaylo@gmail.com>
2023-11-21 00:47:35 +0200

    Create dashboard with charts, add animation for navigation bar, add auto hide for nav-bar on display width < 1200

--- Server/src/main/java/exemple/rest/api/Server/repositories/WeatherDataRepository.java
+++ Server/src/main/java/exemple/rest/api/Server/repositories/WeatherDataRepository.java
@@ -4,9 +4,13 @@ import exemple.rest.api.Server.entity.WeatherData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface WeatherDataRepository extends JpaRepository<WeatherData, Integer> {
     List<WeatherData> findAllByRaining(boolean raining);
+    Integer countWeatherDataByDate(LocalDate date);
 }
